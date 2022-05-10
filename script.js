16830cc046a2b9991f0dd85f2ae3ce11b02fc65e
Kamran <huseynovk355@gmail.com>
2022-05-10 18:12:11 +0400

    changed the code radically, everything works

--- script.js
+++ script.js
@@ -1,72 +1,66 @@
-function computerWeapon() {
-    let weaponDecision = Math.floor(Math.random() * 10)
-    if (weaponDecision <= 9, weaponDecision > 6) {
-        return 'rock';
-    } else if (weaponDecision <= 6, weaponDecision > 3) {
-        return 'scissors';
-    } else if (weaponDecision <= 3 , weaponDecision >= 0) {
-        return 'paper';
-    }
-}
-let computerDecision = computerWeapon()
-let playerScore = 0;
-let computerScore = 0;
-let playerWeapon = prompt('choose your weapon!', '');
-let playerDecision = playerWeapon.toLocaleLowerCase();
+const choices = ['rock', 'paper', 'scissors']
+const winners = []
 
-function playRound(playerDecision, computerDecision, playerScore, computerScore) {
-    if (computerDecision === 'rock' && playerDecision === 'rock') {
-        playerScore += 1;
-        computerScore += 1;
-        return `no winner both of you chose ${playerDecision}`;
-    } else if (computerDecision === 'scissors' && playerDecision === 'scissors') {
-        playerScore += 1;
-        computerScore += 1;
-        return `no winner both of you chose ${playerDecision}`;
-    } else if (computerDecision === 'paper' && playerDecision === 'paper') {
-        playerScore += 1;
-        computerScore += 1;
-        return `no winner both of you chose ${playerDecision}`;
-    } else if (computerDecision === 'rock' && playerDecision === 'paper') {
-        playerScore += 1;
-        return `player won, ${playerDecision} beats ${computerDecision}`;
-    } else if (computerDecision === 'rock' && playerDecision === 'scissors') {
-        computerScore += 1;
-        return `computer won, ${computerDecision} beats ${playerDecision}`;
-    } else if (computerDecision === 'scissors' && playerDecision === 'rock') {
-        playerScore += 1;
-        return `player won, ${playerDecision} beats ${computerDecision}`;
-    } else if (computerDecision === 'scissors' && playerDecision === 'paper') {
-        computerScore += 1;
-        return `computer won, ${computerDecision} beats ${playerDecision}`;
-    } else if (computerDecision === 'paper' && playerDecision === 'scissors') {
-        playerScore += 1;
-        return `player won, ${playerDecision} beats ${computerDecision}`;
-    } else if (computerDecision === 'paper' && playerDecision === 'rock') {
-        computerScore += 1;
-        return `computer won, ${computerDecision} beats ${playerDecision}`;
-    } 
+function game() {
+    for(let i = 1; i <= 5; i++) {
+        playRound(i);
+    }
+    logWins();
 }
 
-function game(playerScore, computerScore, computerDecision, playerDecision) {
-    for (let i = 0; i < 5; i++) {
-        if (playerScore > computerScore){
-            console.log('You won!');
-        } else if (playerScore < computerScore) {
-            console.log('You lost!');
-        } else {
-            console.log('It\'s a tie');
-        }
-        if (i < 5) {
-            computerDecision;
-            playerDecision;
-            playRound(playerDecision, computerDecision, playerScore, computerScore);
-        } else {
-        return 'gameover';
-        }
+function playRound(round) {
+    const playerSelection = playerChoice()
+    const computerSelection = computerChoice()
+    const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner);
+    logRound(playerSelection, computerSelection, winner, round)
+}
+
+function playerChoice() {
+    let input = prompt('choose your weapon!', '');
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    if (check === true){
+        return input;
     }
 }
-console.log (playerDecision)
-console.log (computerDecision)
-console.log (playRound(playerDecision, computerDecision))
-console.log (game(playerScore, computerScore, playerWeapon, playerDecision))
+
+function computerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function validateInput (choice) {
+    return (choices.includes(choice) ? true : false)
+}
+
+function checkWinner(choiceP, choiceC) {
+    if (choiceP === choiceC) {
+        return 'Tie';
+    } else if (choiceP === 'rock' && choiceC === 'scissors' ||
+    choiceP === 'scissors' && choiceC === 'paper' ||
+    choiceP === 'paper' && choiceC === 'rock') {
+        return 'Player';
+    } else {
+        return 'Computer';
+    }
+}
+
+function logRound (playerChoice, computerChoice, winner, round) {
+    console.log('Round', round)
+    console.log('Player chose:', playerChoice)
+    console.log('Computer chose', computerChoice)
+    console.log(winner, "Won the round!")
+    console.log('--------------------------------------------');
+}
+
+function logWins () {
+    let playerWins = winners.filter((item) => item === 'Player').length;
+    let computerWins = winners.filter((item) => item === 'Computer').length;
+    let ties = winners.filter((item) => item === 'Tie').length;
+    console.log('Results: ');
+    console.log('Player Wins: ', playerWins);
+    console.log('Computer Wins: ', computerWins);
+    console.log('Ties: ', ties);
+}
+
+game();
