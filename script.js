2f4dc778abff9d621975c0f1e51012b4b649f592
Kamran <huseynovk355@gmail.com>
2022-05-26 20:47:46 +0400

    reformed the game to work with buttons instead of prompt

--- script.js
+++ script.js
@@ -1,66 +1,87 @@
-const choices = ['rock', 'paper', 'scissors']
-const winners = []
+//let computer choose a random number of an array which has one of 3 choices
+//player choice depends on the button pressed 
+//log the winner into a seperate div
+choices = ['rock', 'paper', 'scissors'];
+winners = [];
+
+let playerSelection;
+let computerSelection = computerChoice();
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('#result');
+const cscore = document.querySelector('.cscore')
+const pscore = document.querySelector('.pscore')
+
+pscore.textContent = `Player: 0`
+cscore.textContent = `Opponent: 0`
+result.textContent = 'Lets begin!'
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        playerSelection = btn.className;
+        computerSelection = computerChoice();
+        playRound(playerSelection, computerSelection);
+        game();
+    });
+});
 
 function game() {
-    for(let i = 1; i <= 5; i++) {
-        playRound(i);
-    }
-    logWins();
-}
-
-function playRound(round) {
-    const playerSelection = playerChoice()
-    const computerSelection = computerChoice()
-    const winner = checkWinner(playerSelection, computerSelection);
-    winners.push(winner);
-    logRound(playerSelection, computerSelection, winner, round)
-}
-
-function playerChoice() {
-    let input = prompt('choose your weapon!', '');
-    input = input.toLowerCase();
-    let check = validateInput(input);
-    if (check === true){
-        return input;
+    let i = winners.length;
+    if (i == 5) {
+        if (playerScore > computerScore) {
+            result.textContent = `Game over! You won!`;
+            winners = [];
+            playerScore = 0;
+            computerScore = 0;
+        } else if (playerScore === computerScore) {
+            result.textContent = `Game over! Somehow it's a tie.`;
+            winners = [];
+            playerScore = 0;
+            computerScore = 0;
+        } else {
+            result.textContent = `Game over! You lost...`;
+            winners = [];
+            playerScore = 0;
+            computerScore = 0;
+        }
     }
 }
 
 function computerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
+    
 }
 
-function validateInput (choice) {
-    return (choices.includes(choice) ? true : false)
-}
+function playRound(P, C) {
+    if (P == 'rock' && C == 'scissors' ||
+        P == 'scissors' && C == 'paper' ||
+        P == 'paper' && C == 'rock') {
+        result.textContent = `You won! Opponent chose ${C}.`;
+        winners.push('P');
+        playerScore += 1;
+        pscore.textContent = `Player: ${playerScore}`;
+        cscore.textContent = `Opponent: ${computerScore}`;
 
-function checkWinner(choiceP, choiceC) {
-    if (choiceP === choiceC) {
-        return 'Tie';
-    } else if (choiceP === 'rock' && choiceC === 'scissors' ||
-    choiceP === 'scissors' && choiceC === 'paper' ||
-    choiceP === 'paper' && choiceC === 'rock') {
-        return 'Player';
+        return;
+    } else if (P == C) {
+        result.textContent = `Tie. Opponent chose ${C}.`;
+        winners.push('N');
+        pscore.textContent = `Player: ${playerScore}`;
+        cscore.textContent = `Opponent: ${computerScore}`;
+
+        return;
     } else {
-        return 'Computer';
+        result.textContent = `You lost! Opponent chose ${C}.`;
+        winners.push('C');
+        computerScore += 1;
+        pscore.textContent = `Player: ${playerScore}`;
+        cscore.textContent = `Opponent: ${computerScore}`;
+
+        return;
     }
 }
 
-function logRound (playerChoice, computerChoice, winner, round) {
-    console.log('Round', round);
-    console.log('Player chose:', playerChoice);
-    console.log('Computer chose', computerChoice);
-    console.log(winner, "Won the round!");
-    console.log('--------------------------------------------');
-}
 
-function logWins () {
-    let playerWins = winners.filter((item) => item === 'Player').length;
-    let computerWins = winners.filter((item) => item === 'Computer').length;
-    let ties = winners.filter((item) => item === 'Tie').length;
-    console.log('Results: ');
-    console.log('Player Wins: ', playerWins);
-    console.log('Computer Wins: ', computerWins);
-    console.log('Ties: ', ties);
-}
 
-game();
